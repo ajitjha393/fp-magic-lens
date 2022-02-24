520091fe4cb2168da48d3b9adff9cca87551cb67
ajitjha393 <ajitjha393@gmail.com>
2022-02-24 17:11:44 +0530

    add publish script

---
+++ publish.js
@@ -0,0 +1,27 @@
+// @ts-check
+const sh = require("shelljs")
+const rl = require("readline-sync");
+
+if (!rl.keyInYNStrict("you sure?")) process.exit();
+
+sh.ls().includes("dist") && sh.rm("-r", "dist");
+sh.exec("yarn tsc")
+sh.cp("LICENSE", "dist/LICENSE")
+sh.cp("README.md", "dist/README.md");
+sh.cp("package.json", "dist/package.json");
+sh.cd("dist");
+sh.echo([
+  "*.spec.js",
+  "*.spec.d.ts",
+  "*.test.js",
+  "*.test.d.ts"
+].join("\n")).to(".npmignore")
+sh.exec("npm publish")
+sh.exec(`git tag ${
+  sh.cat("package.json")
+  .grep("version")
+  .sed(/.*"version": "(.*)".*/, "v$1")
+  .toString()
+}`)
+sh.cd("..")
+sh.rm("-r", "dist")
